cd853705bbdd267aedb5d11089d20fb581009e7e
tjray-dev <tate.ray92@gmail.com>
2022-01-20 14:03:00 -0700

    fleshed out Health and harvest report migrations; altered plants migration

--- db/migrate/20220116152527_create_harvest_reports.rb
+++ db/migrate/20220116152527_create_harvest_reports.rb
@@ -2,7 +2,12 @@ class CreateHarvestReports < ActiveRecord::Migration[6.1]
   def change
     create_table :harvest_reports do |t|
       t.integer :height
-      t.integer :weight
+      t.integer :weight_wet_in_grams
+      t.integer :weight_dry_in_grams
+      t.integer :total_wattage_used
+      t.integer :total_water_used_in_milileters
+      t.integer :water_per_gram_in_milileters
+      t.integer :wattage_per_gram
       t.integer :plant_id
 
       t.timestamps
--- db/migrate/20220116152710_create_health_reports.rb
+++ db/migrate/20220116152710_create_health_reports.rb
@@ -3,6 +3,13 @@ class CreateHealthReports < ActiveRecord::Migration[6.1]
     create_table :health_reports do |t|
       t.integer :plant_id
       t.string :plant_image
+      t.integer :height_in_centimeters
+      t.integer :air_humidity
+      t.integer :light_schedule
+      t.integer :day_air_temp
+      t.integer :night_air_temp
+      t.integer :watering_volume_per_day
+      t.integer :container_size
       t.integer :ph
       t.text :diagnosis
       t.text :comments
