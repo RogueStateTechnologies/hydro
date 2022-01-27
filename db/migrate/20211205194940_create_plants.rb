ac5008f55268f36bcec5ddc63fea7036bdf8fe48
tjray-dev <tate.ray92@gmail.com>
2022-01-27 11:53:27 -0700

    added user to plant association

--- db/migrate/20211205194940_create_plants.rb
+++ db/migrate/20211205194940_create_plants.rb
@@ -2,6 +2,7 @@ class CreatePlants < ActiveRecord::Migration[6.1]
   def change
     create_table :plants do |t|
 
+      t.integer :user_id
       t.integer :crop_id
       t.integer :variant_id
       t.integer :medium_id
