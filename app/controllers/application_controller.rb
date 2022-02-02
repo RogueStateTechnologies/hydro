151826988a9e75a4a988269c8809e449fcb00cef
tjray-dev <tate.ray92@gmail.com>
2022-02-02 15:48:17 -0700

    remoeved skip action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  skip_before_action :verify_authenticity_token
+
 
   def after_sign_in_path_for(resource)
     user_plants_path(current_user) # your path
