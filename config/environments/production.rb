4d602a8880f66295cae14fa8ef8364726e0a330d
tjray-dev <tate.ray92@gmail.com>
2022-02-07 14:05:00 -0700

    added config settings to production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,21 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "hydro_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.default_url_options = { :host => 'http://myapp.herokuapp.com' }
+
+  config.action_mailer.smtp_settings = {
+    :user_name => "apikey",
+    :password => ENV["SENDGRID_API_KEY"],
+    :address => 'smtp.sendgrid.net',
+    :domain => 'myapp.herokuapp.com',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+}
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
