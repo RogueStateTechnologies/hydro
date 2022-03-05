cc2c8dd0fe54af3e8c592f8f9bf4e73ac2b6e0b7
tjray-dev <tate.ray92@gmail.com>
2022-03-05 15:06:07 -0700

    typo hunting round four

--- app/helpers/my_form_builder.rb
+++ app/helpers/my_form_builder.rb
@@ -1,4 +1,4 @@
-class MyFormBuilder < Action::Helpers::FormBuilder
+class MyFormBuilder < ActionPack::Helpers::FormBuilder
 
   def section(title, id = "section-#{SecureRandom.uuid}")
     @template.content_tag(:label, title, {class: 'form-label', id: id}) +
