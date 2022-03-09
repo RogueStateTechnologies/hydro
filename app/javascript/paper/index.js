a0c1e60b852ea46300a1f4afe16b868e39072771
tjray-dev <tate.ray92@gmail.com>
2022-03-08 22:03:24 -0700

    still trying to get webpack working

--- app/javascript/paper/index.js
+++ app/javascript/paper/index.js
@@ -1,2 +1,15 @@
 const paper = require.context('./', true, /\*.js$/)
 paper.keys().forEach(paper)
+
+// import popper from './core/popper.min.js';
+// import jquery from './core/jquery.min.js';
+// import bootstrap from './core/bootstrap.min.js';
+// import daygrid from './plugins/fullcalendar/daygrid.min.js';
+// import fullcalendar from './plugins/fullcalendar/fullcalendar.min.js';
+// import interaction from './plugins/fullcalendar/interaction.min.js'
+// import moment from './plugins/moment.min.js'
+// import bootstrapDateTimePicker from './plugins/bootstrap-datetimepicker.js';
+// import bootstrapNotify from './plugins/bootstrap-notify.js' 
+// import paperDashboard from './paper-dashboard.js';
+// import paperDashboardMap from './paper-dashboard.js.map';
+// import paperDashboardMin from './paper-dashboard.min.js';
