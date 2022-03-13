27a42b99df09c83dc4e3586fb0287f8572be6ad2
Jim Ray <jim.ray@xplortechnologies.com>
2022-03-13 10:46:45 -0600

    use relations for finds and forms

--- app/controllers/variants_controller.rb
+++ app/controllers/variants_controller.rb
@@ -1,29 +1,47 @@
 # frozen_string_literal: true
 
 class VariantsController < ApplicationController
+  before_action :find_crop, except: [:index]
   before_action :find_variant, except: [:index, :new, :create]
 
   def index
-    @variants = Variant.all
+    @variants = @crop.variants.all
   end
 
-  def show
-    @varient
-  end
+  def show; end
   
-  def new
-    @variant = Variant.new
-  end
+  def new; end
 
   def create
-    @variant = Variant.create(variant_params)
+    @variant = @crop.variants.new(variant_params)
+    if @variant.save
+      redirect_to crops_path, flash: { notice: "Variant created" }
+    end
   end
 
   def update
-    @variant.update(variant_params)
+    if @variant.update(variant_params)
+      redirect_to crops_path
+    end
   end
 
   def delete
-    @variant.delete
+    if @variant.destroy
+      redirect_to crops_path
+    end
+  end
+
+  private
+
+  def find_crop
+    @crop = Crop.find(params[:crop_id])
+  end
+
+  def find_variant
+    @variant = params[:id] ? @crop.variants.find(params[:id]) : @crop.variants.new
+  end
+
+  def variant_params
+    params.require(:variant).permit()
   end
 end
