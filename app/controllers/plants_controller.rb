a217c6e72781e75020ef2796d3388aeb352f9c3b
Jim Ray <jim.ray@xplortechnologies.com>
2022-03-13 10:46:45 -0600

    use relations for finds and forms

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -2,26 +2,37 @@
 
 class PlantsController < ApplicationController
   # skip_before_action :verify_authenticity_token
-  before_action :find_user
   before_action :find_plant, except: [:index, :new, :create]
 
 
   def index
-    @plants = @user.plants.all
+    @plants = current_user.plants.all
   end
 
   def show
-    @plant
   end
   
   def new
-    @plant = Plant.new
+    @plant = current_user.plants.new
   end
 
   def create
-    @plant = @user.plants.create(plant_params)
-    # mail = NotifierMailer.welcome(@user)
-    # mail.deliver_now
-    redirect_to user_plant_path(@user, @plant)
+    @plant = current_user.plants.new(plant_params)
+    if @plant.save!
+      # NotifierMailer.welcome(@user).deliver_now
+      redirect_to user_plant_path(@user, @plant)
+    end
   end
+
+  private
+
+  def find_plant
+    @plant = current_user.plants.find(params[:id])
+  end
+
+  def plant_params
+    params.require(:plant).permit(:user_id, :crop_id, :start_date, :variant_id, :medium_id,
+                                  :plan_id, :phase_id)
+    end
+
 end
