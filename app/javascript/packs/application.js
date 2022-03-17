7a1ea51898ab3a0b03debb7a0f4e77f6c0576782
tjray-dev <tate.ray92@gmail.com>
2022-03-17 17:34:24 -0600

    react is reacting!

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,3 +24,7 @@ document.addEventListener("turbolinks:load", () =>{
 function displayChart() {
   
 }
+// Support component names relative to this directory:
+var componentRequireContext = require.context("components", true);
+var ReactRailsUJS = require("react_ujs");
+ReactRailsUJS.useContext(componentRequireContext);
