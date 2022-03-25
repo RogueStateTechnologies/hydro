c7d4915bbb2a315a390715844c786ce5821e419b
tjray-dev <tate.ray92@gmail.com>
2022-03-24 18:22:41 -0600

    attempting to get aws s3 working on heroku deployment(attampt 13)

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
 
+web: bundle exec puma -C config/puma.rb
+worker: bundle exec sidekiq -e production -t 25 -C config/sidekiq.yml
--- config/puma.rb
+++ config/puma.rb
@@ -26,6 +26,25 @@ environment ENV.fetch('RAILS_ENV', 'development')
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch('PIDFILE', 'tmp/pids/server.pid')
 
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+threads threads_count, threads_count
+port        ENV.fetch("PORT") { 3000 }
+environment ENV.fetch("RAILS_ENV") { "development" }
+workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+preload_app!
+
+before_fork do 
+    @sidekiq_pid ||= spawn('bundle exec sidekiq -t 25')
+end
+
+on_worker_boot do
+    ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+end
+
+on_restart do
+    Sidekiq.redis.shutdown { |conn| conn.close }
+end
+
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked web server processes. If using threads and workers together
 # the concurrency of the application would be max `threads` * `workers`.
