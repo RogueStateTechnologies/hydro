65c967788e07a209274a8a73394357d74c3eaf81
tjray-dev <tate.ray92@gmail.com>
2022-03-27 20:18:41 -0600

    email confirmation deployment (attempt 1)

--- db/migrate/20220122015922_devise_create_users.rb
+++ db/migrate/20220122015922_devise_create_users.rb
@@ -22,18 +22,16 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       # t.string   :last_sign_in_ip
 
       ## Confirmable
-      # t.string   :confirmation_token
-      # t.datetime :confirmed_at
-      # t.datetime :confirmation_sent_at
-      # t.string   :unconfirmed_email # Only if using reconfirmable
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+      t.string   :unconfirmed_email # Only if using reconfirmable
 
       ## Lockable
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
-
-      # should have a name column
-      # should have a sub table of favorite crops, variants, etc. to make form completion
+      
       t.timestamps null: false
     end
 
