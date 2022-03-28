8b480911964cbc96a5baec90b6bd7f54c85b880b
tjray-dev <tate.ray92@gmail.com>
2022-03-27 21:37:07 -0600

    email confirmation deployment (attempt 5)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
-  config.action_mailer.default_url_options = { :host => 'smtp.sendgrid.net' }
+  config.action_mailer.default_url_options = { :host => 'sheltered-castle-86259.herokuapp.com' }
 
   ActionMailer::Base.smtp_settings = {
     :user_name            => ENV['SENDGRID_USERNAME'],
