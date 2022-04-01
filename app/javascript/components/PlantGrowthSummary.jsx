fb31f25c280895cf4a9945149d4e065a4383cfda
tjray-dev <tate.ray92@gmail.com>
2022-04-01 10:31:45 -0600

    basic fomatting for growht and plan partials on the plan#show

---
+++ app/javascript/components/PlantGrowthSummary.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const PlantGrowthSummary = () => {
+  return (
+    <div className="card-body">
+      <div className="nav-tabs-navigation">
+        <div className="nav-tabs-wrapper">
+          <ul id="tabs" className="nav nav-tabs" role="tablist">
+            <li className="nav-item">
+              <a className="nav-link active" data-toggle="tab" href="#home" role="tab" aria-expanded="true">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" data-toggle="tab" href="#profile" role="tab" aria-expanded="false">Profile</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" data-toggle="tab" href="#messages" role="tab" aria-expanded="false">Messages</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div id="my-tab-content" className="tab-content text-center">
+        <div className="tab-pane active" id="home" role="tabpanel" aria-expanded="true">
+          <p>
+            Larger, yet dramatically thinner. More powerful, but remarkably power efficient. With a smooth metal surface that seamlessly meets the new Retina HD display.
+          </p>
+        </div>
+        <div className="tab-pane" id="profile" role="tabpanel" aria-expanded="false">
+          <p>
+            Here is your profile.
+          </p>
+        </div>
+        <div className="tab-pane" id="messages" role="tabpanel" aria-expanded="false">
+          <p>
+            Here are your messages.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default PlantGrowthSummary
